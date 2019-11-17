50af797382755cfff21e07e879a25ce03d85a406
Michele_Manco <motlamik@gmail.com>
2019-11-17 22:57:26 +0100

    exer beginned

---
+++ main.js
@@ -0,0 +1,15 @@
+var km =parseInt( prompt('quanti km devi percorrere?'));
+console.log(km);
+var anni = parseInt(prompt('quanti anni hai?'));
+console.log(anni);
+var prezzo = km * 0.21;
+console.log(prezzo);
+if (anni< 18){
+  var scontominori = km * 0.168;
+  console.log(scontominori);
+}
+else (anni > 65){
+  var scontoargento = km * 0.126;
+  console.log(scontoargento);
+
+}
